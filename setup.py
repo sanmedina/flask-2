3c2b661151d822d9e9e4bb6ec457c0b070baddfc
Santiago Medina <devsanmedina@protonmail.com>
2018-08-20 18:11:26 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='my_app',
+    version='1.0.0',
+    license='GNU General Public License V3',
+    author='Dev Medina',
+    author_email='devsanmedina@protonmail.com',
+    description='Hello world app for Flask',
+    packages=find_packages(),
+    platforms='any',
+    install_requires=['flask'],
+    extras_require={
+        'dev': [
+            'pylint',
+            'autopep8',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
