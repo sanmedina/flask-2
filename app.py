878ef3ceaf14ed0e45f122e262ce79dd50b1d6fa
Santiago Medina <devsanmedina@protonmail.com>
2018-08-20 16:27:51 -0500

    load config from instance folder

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
+
 from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 # Custom static
 # app = Flask(
 #     __name__,
@@ -8,7 +10,14 @@ app = Flask(__name__)
 #     static_folder='static'
 # )
 # Class config
-app.config.from_object('config.DevelopmentConfig')
+# app.config.from_object('config.DevelopmentConfig')
+# Load from instance
+app = Flask(__name__,
+            instance_path=os.path.dirname(__file__) + '/instance',
+            instance_relative_config=True)
+app.config.from_pyfile('config.cfg')
+# Supress error
+# app.config.from_pyfile('config.cfg', silent=True)
 
 
 @app.route('/')
