67d9060d07939d9e45319361dcdb6b9969c11c6c
Santiago Medina <devsanmedina@protonmail.com>
2018-08-20 17:03:15 -0500

    use hello module as a blueprint

--- my_app/hello/views.py
+++ my_app/hello/views.py
@@ -1,19 +1,22 @@
-from my_app import app
+from flask import Blueprint
+
 from my_app.hello.models import MESSAGESS
 
+hello = Blueprint('hello', __name__)
 
-@app.route('/')
-@app.route('/hello')
+
+@hello.route('/')
+@hello.route('/hello')
 def hello_world():
     return MESSAGESS['default']
 
 
-@app.route('/show/<key>')
+@hello.route('/show/<key>')
 def get_message(key):
     return MESSAGESS.get(key, '{} not found!'.format(key))
 
 
-@app.route('/add/<key>/<message>')
+@hello.route('/add/<key>/<message>')
 def add_or_update_message(key, message):
     MESSAGESS[key] = message
     return '{} Added/Updated'.format(key)
