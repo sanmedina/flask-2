9230afbea942e3105e428d28e73ab393a3071de3
Santiago Medina <devsanmedina@protonmail.com>
2018-08-20 17:14:29 -0500

    move static sample to module

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask import url_for
 
 from my_app.hello.views import hello
 
 app = Flask(__name__)
+# Custom static
+# app = Flask(
+#     __name__,
+#     static_url_path='/otherstatic',
+#     static_folder='static'
+# )
 app.register_blueprint(hello)
+
+@app.route('/logo')
+def logo():
+    url = url_for('static', filename='images/logo.png')
+    return '<img src="{}">'.format(url)
