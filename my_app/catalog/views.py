b8804dc86fa99b285cedc780856c42d001d33ebf
Santiago Medina <devsanmedina@protonmail.com>
2018-09-01 12:15:50 -0500

    tried to hande XHR with a decorator

--- my_app/catalog/views.py
+++ my_app/catalog/views.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from functools import wraps
 
 from flask import Blueprint
 from flask import jsonify
@@ -14,8 +15,29 @@ from my_app.catalog.models import Product
 catalog = Blueprint('catalog', __name__)
 
 
+# Does not work
+def template_or_json(template=None):
+    '''
+    Return a dict from your view and this will either pass it to a template or
+    render a json. Use like:
+
+    @template_or_json('template.html')
+    '''
+    def decorated(f):
+        @wraps(f)
+        def decorated_fn(*args, **kwargs):
+            ctx = f(*args, **kwargs)
+            if request.is_xhr or not template:
+                return jsonify(ctx)
+            else:
+                return render_template(template, **ctx)
+        return decorated_fn
+    return decorated
+
+
 @catalog.route('/')
 @catalog.route('/home')
+# @template_or_json('home.html') # Does not work
 def home():
     if request.is_xhr:
         products = Product.query.all()
