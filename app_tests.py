08157120e75740e4c408ff8a857a57642994363f
Santiago Medina <devsanmedina@protonmail.com>
2018-09-17 20:35:20 -0500

    add products test

--- app_tests.py
+++ app_tests.py
@@ -11,6 +11,7 @@ class CatalogTestCase(unittest.TestCase):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
             self.test_db_file
         app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
         self.app = app.test_client()
         db.create_all()
 
@@ -20,6 +21,13 @@ class CatalogTestCase(unittest.TestCase):
     def test_home(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status_code, 200)
+
+    def test_products(self):
+        "Test Products list page"
+        rv = self.app.get('/products')
+        self.assertEqual(rv.status_code, 200)
+        self.assertTrue('No previous page'.encode() in rv.data)
+        self.assertTrue('No next page'.encode() in rv.data)
 
 
 if __name__ == '__main__':
