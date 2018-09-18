c6428548b4607828eeb706cb67df08b630bab6d0
Santiago Medina <devsanmedina@protonmail.com>
2018-09-17 20:29:08 -0500

    add home test

---
+++ app_tests.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+import tempfile
+
+from my_app import app, db
+
+
+class CatalogTestCase(unittest.TestCase):
+    def setUp(self):
+        self.test_db_file = tempfile.mkstemp()[1]
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+            self.test_db_file
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        os.remove(self.test_db_file)
+
+    def test_home(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
